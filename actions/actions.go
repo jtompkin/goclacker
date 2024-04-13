db082f5fb93c6e5932f6300fcc7ecef2ea3126a5
jtompkin <jtompkin-dev@pm.me>
2024-04-12 21:48:07 -0400

    Stuff

--- actions/actions.go
+++ actions/actions.go
@@ -1,8 +1,70 @@
 package actions
 
-import "github.com/jtompkin/goclacker/stack"
+import (
+	"fmt"
+	"math"
 
-func add(stkOp *stack.StackOperator) {
-    stkOp.stack.push(stkOp.stack.pop() + stkOp.stack.pop())
-    stkOp.operators
+	"github.com/jtompkin/goclacker/stack"
+)
+
+func Add(stkOp *stack.StackOperator) error {
+	stkOp.Stack.Push(stkOp.Stack.Pop() + stkOp.Stack.Pop())
+	stkOp.Stack.Display()
+	return nil
+}
+
+func Subtract(stkOp *stack.StackOperator) error {
+	x := stkOp.Stack.Pop()
+	y := stkOp.Stack.Pop()
+	stkOp.Stack.Push(y - x)
+	stkOp.Stack.Display()
+	return nil
+}
+
+func Multiply(stkOp *stack.StackOperator) error {
+	stkOp.Stack.Push(stkOp.Stack.Pop() * stkOp.Stack.Pop())
+	stkOp.Stack.Display()
+	return nil
+}
+
+func Divide(stkOp *stack.StackOperator) error {
+	divisor := stkOp.Stack.Pop()
+	if divisor == 0 {
+		return stkOp.Fail("cannot divde by 0", divisor)
+	}
+	dividend := stkOp.Stack.Pop()
+	stkOp.Stack.Push(dividend / divisor)
+	stkOp.Stack.Display()
+	return nil
+}
+
+func Power(stkOp *stack.StackOperator) error {
+	exponent := stkOp.Stack.Pop()
+	base := stkOp.Stack.Pop()
+	if base == 0 && exponent < 0 {
+		return stkOp.Fail("cannot raise 0 to negative power", base, exponent)
+	}
+	if base < 0 && exponent != float64(int(exponent)) {
+		return stkOp.Fail("cannot raise negative number to non-integer power", base, exponent)
+	}
+	stkOp.Stack.Push(math.Pow(base, exponent))
+    stkOp.Stack.Display()
+	return nil
+}
+
+func Display(stkOp *stack.StackOperator) error {
+	stkOp.Stack.Display()
+	return nil
+}
+
+func Pop(stkOp *stack.StackOperator) error {
+	stkOp.Stack.Pop()
+	stkOp.Stack.Display()
+	return nil
+}
+
+func Clear(stkOp *stack.StackOperator) error {
+	fmt.Printf("cleared %d values\n", stkOp.Stack.Len())
+	stkOp.Stack.SetValues(make([]float64, 0, stkOp.MaxStack))
+	return nil
 }
--- stack/stack.go
+++ stack/stack.go
@@ -1,73 +1,123 @@
 package stack
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
+// Contains a slice of values and methods to operate on that slice.
 type Stack struct {
-	values []float32
+	values []float64
 }
 
-func (stk *Stack) pop() float32 {
+func (stk *Stack) Pop() float64 {
 	n := len(stk.values) - 1
 	f := stk.values[n]
 	stk.values = stk.values[:n]
 	return f
 }
 
-func (stk *Stack) push(f float32) {
+func (stk *Stack) Push(f float64) error {
+	if len(stk.values)+1 > cap(stk.values) {
+		return errors.New(fmt.Sprintf("cannot push %f, stack at capacity", f))
+	}
 	stk.values = append(stk.values, f)
+	return nil
 }
 
-func (stk *Stack) display() {
+func (stk *Stack) Display() {
 	fmt.Println(stk.values)
 }
 
-func NewStack(values []float32) *Stack {
+func (stk *Stack) SetValues(values []float64) {
+	stk.values = values
+}
+
+func (stk *Stack) Len() int {
+	return len(stk.values)
+}
+
+func NewStack(values []float64) *Stack {
 	stk := Stack{}
 	stk.values = values
 	return &stk
 }
 
+// Contains a function to operate on a StackOperator instance. `Pops` and
+// `Pushes` represent the number of values the functions pops from and pushes to
+// the stack.
 type Operation struct {
-	action func(*StackOperator)
-	pops   int
-	pushes int
+	action func(*StackOperator) error
+	Pops   int
+	Pushes int
 }
 
-func NewOperation(action func(*StackOperator), pops int, pushes int) *Operation {
-	op := Operation{}
-	op.action = action
-	op.pops = pops
-	op.pushes = pushes
+func NewOperation(action func(*StackOperator) error, pops int, pushes int) *Operation {
+	op := Operation{action: action, Pops: pops, Pushes: pushes}
 	return &op
 }
 
+// Contains map for converting string tokens into operations that can be called
+// to operate on the stack.
 type StackOperator struct {
 	operators map[string]*Operation
-	stack     Stack
+	Stack     Stack
+	MaxStack  int
 }
 
 func (stkOp *StackOperator) ParseInput(input string) {
-
+	split := strings.Split(input, " ")
+	for i, s := range split {
+		var pErr error
+		f, err := strconv.ParseFloat(s, 64)
+		if err != nil {
+			pErr = stkOp.ParseToken(s)
+		} else {
+			pErr = stkOp.Stack.Push(f)
+			if i == len(split)-1 {
+				stkOp.Stack.Display()
+			}
+		}
+		if pErr != nil {
+			fmt.Fprintf(os.Stderr, "operation error: %s\n", pErr)
+		}
+	}
 }
 
-func (stkOp *StackOperator) ParseToken(token string) {
+func (stkOp *StackOperator) ParseToken(token string) error {
 	op := stkOp.operators[token]
 	if op == nil {
-		return
+		return nil
 	}
-	stkLen := len(stkOp.stack.values)
-	if stkLen < op.pops {
-		return
+	stkLen := stkOp.Stack.Len()
+	if stkLen < op.Pops {
+		return stkOp.Fail(fmt.Sprintf("operation needs %d values in stack", op.Pops))
 	}
-	if stkLen-op.pops+op.pushes > cap(stkOp.stack.values) {
-		return
+	if stkLen-op.Pops+op.Pushes > stkOp.MaxStack {
+		return stkOp.Fail("operation would overflow stack")
 	}
-	op.action(stkOp)
+	if err := op.action(stkOp); err != nil {
+		return stkOp.Fail(fmt.Sprintf("%s", err))
+	}
+	return nil
+}
+
+// Pushes all `values` to the stack and returns an error containing `message`
+func (stkOp *StackOperator) Fail(message string, values ...float64) error {
+	for _, value := range values {
+		stkOp.Stack.Push(value)
+	}
+	return errors.New(message)
 }
 
 func NewStackOperator(operators map[string]*Operation, maxStack int) *StackOperator {
-	stkOp := StackOperator{}
-	stkOp.operators = operators
-	stkOp.stack = *NewStack(make([]float32, 0, maxStack))
+	stkOp := StackOperator{
+		operators: operators,
+		MaxStack:  maxStack,
+		Stack:     *NewStack(make([]float64, 0, maxStack)),
+	}
 	return &stkOp
 }
