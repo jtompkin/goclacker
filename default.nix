f5fd1898444af4d3d2642c50e07ee7b05a6b389a
jtompkin <jtompkin-dev@pm.me>
2025-04-04 15:06:15 -0400

    update nix version

--- default.nix
+++ default.nix
@@ -2,12 +2,12 @@ with import (fetchTarball
   "https://github.com/NixOS/nixpkgs/archive/2bfc080955153be0be56724be6fa5477b4eefabb.tar.gz") {};
   buildGoModule rec {
     pname = "goclacker";
-    version = "1.4.1";
+    version = "1.4.2";
     src = fetchFromGitHub {
       owner = "jtompkin";
       repo = "goclacker";
       rev = "v${version}";
-      hash = "sha256-+kJWNFlSgrgv49RT9/AHWfj1rHXgna7JphN+Be2pNpw=";
+      hash = "sha256-3jELTnPFDpB5vJ+mCTV6drYIvhiPIhmQmsn2MlvaNz0=";
     };
     vendorHash = "sha256-rELkSYwqfMFX++w6e7/7suzPaB91GhbqFsLaYCeeIm4=";
   }
