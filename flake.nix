44007d11881a402ad97df1b1e99d8a46cf2ba8b5
jtompkin <jtompkin-dev@pm.me>
2025-06-06 10:44:47 -0400

    change system handling in flake

--- flake.nix
+++ flake.nix
@@ -9,43 +9,54 @@
     { self, nixpkgs, ... }:
     let
       inherit (nixpkgs) lib;
-      systems = [
+      allSystems = [
         "x86_64-linux"
         "aarch64-linux"
         "x86_64-darwin"
         "aarch64-darwin"
       ];
-      forAllSystems = lib.genAttrs systems;
+      forAllSystems =
+        f:
+        lib.genAttrs allSystems (
+          system:
+          f {
+            inherit system;
+            pkgs = import nixpkgs { inherit system; };
+          }
+        );
     in
     {
-      packages = forAllSystems (system: {
-        goclacker = nixpkgs.legacyPackages.${system}.callPackage ./. { };
-        default = self.packages.${system}.goclacker;
-      });
-      apps = forAllSystems (system: {
-        goclacker = {
-          type = "app";
-          program = lib.getExe self.packages.${system}.goclacker;
-          meta.description = "Command line reverse Polish notation calculator";
-        };
-        default = self.apps.${system}.goclacker;
-      });
-      overlays = {
-        goclacker = final: prev: {
-          goclacker = final.callPackage ./. { };
-        };
-        default = self.overlays.goclacker;
-      };
+      packages = forAllSystems (
+        { pkgs, system }:
+        {
+          goclacker = pkgs.callPackage self { };
+          default = self.packages.${system}.goclacker;
+        }
+      );
+      apps = forAllSystems (
+        { system, ... }:
+        {
+          goclacker = {
+            type = "app";
+            program = lib.getExe self.packages.${system}.goclacker;
+            meta.description = "Command line reverse Polish notation calculator";
+          };
+          default = self.apps.${system}.goclacker;
+        }
+      );
       devShells = forAllSystems (
-        system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
+        { pkgs, system }:
         {
           goclacker = pkgs.mkShell { packages = [ pkgs.go ]; };
           default = self.devShells.${system}.goclacker;
         }
       );
+      overlays = {
+        goclacker = final: prev: {
+          goclacker = final.callPackage self { };
+        };
+        default = self.overlays.goclacker;
+      };
       homeModules = {
         goclacker =
           { pkgs, ... }:
